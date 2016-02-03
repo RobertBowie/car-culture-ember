6209729753930684772fbd4a2f9994dbf2559e99
RobertBowie <robertb1903@gmail.com>
2016-02-02 17:37:25 -0800

    Added session initializer and rudimentary login.

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,10 +4,20 @@ export default Ember.Route.extend({
   actions: {
     displayAlert: function(text) {
       this._displayAlert(text);
+    },
+
+    login: function() {
+      var controller = this;
+      controller.get("session").login().then(function(user) {
+        // Persist your users details.
+      }, function() {
+        // User rejected authentication request
+      });
     }
   },
 
   _displayAlert: function(text) {
     alert(text);
-  }
+  },
+
 });
