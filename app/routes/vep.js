5f2cadffc2f1706fd70dc92318980dd06c818323
RobertBowie <robertb1903@gmail.com>
2016-02-06 14:31:48 -0800

    Cleans up index, moves login/logout to navbar.

--- app/routes/vep.js
+++ app/routes/vep.js
@@ -1,6 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  actions: {
+    logOut: function() {
+      var controller = this;
+      controller.get("session").logOut().then(function() {
+        console.log("Logged Out");
+      });
+    }
+  },
+
   model() {
     return this.store.findAll('vehicle');
   }
